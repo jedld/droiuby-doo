4e09fa02b83598a3c98fe2bc2f25a0c2aba76796
Joseph <joseph@Froze.local>
2014-02-20 18:00:49 +0800

    checked_change bug fixes

--- lib/droiuby/support/fixnum.rb
+++ lib/droiuby/support/fixnum.rb
@@ -1,4 +1,4 @@
-class Integer
+class Fixnum
 
   def to_dp
     Java::com.droiuby.client.core.builder.ActivityBuilder.toDeviceIndependentPixels(_current_activity, self)    
--- lib/droiuby/wrappers/compound_button_wrapper.rb
+++ lib/droiuby/wrappers/compound_button_wrapper.rb
@@ -6,8 +6,8 @@ class CompoundButtonWrapper < ViewWrapper
   def on(event,&block)
     listener_ref = event.to_s.camelize
     case event.to_sym
-      when :checked_changed
-        self.native.send(:"setOn#{listener_ref}Listener",_listener("On#{listener_ref}Listener", 'android.widget.CompundButton', &block))
+      when :checked_change
+        self.native.send(:"setOn#{listener_ref}Listener",_listener("On#{listener_ref}Listener", 'android.widget.CompoundButton', &block))
       else
         super(event, &block)
     end
