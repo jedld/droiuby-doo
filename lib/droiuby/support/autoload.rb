0ee8d9a82c121bdc3c8cad44e337e37cf891b9b8
Joseph <joseph@Froze.local>
2014-02-22 22:55:47 +0800

    fix autoload WIP

--- lib/droiuby/support/autoload.rb
+++ lib/droiuby/support/autoload.rb
@@ -1,3 +1,4 @@
+$autoload_path = [nil,File.join('droiuby','wrappers')]
 
 class Object
   class << self
@@ -10,12 +11,26 @@ class Object
       @looked_for[str_name] = 1
       
       name_parts = name.to_s.split('::').collect { |n| n.underscore }
-      require_path = File.join(*name_parts)
       
-      puts "autoloading #{require_path}"
-      require require_path
-      klass = const_get(name)
-      return klass if klass
+      
+      $autoload_path.each do |path|
+        path_array = unless path.nil? 
+          [path] + name_parts
+        else
+          name_parts
+        end 
+        require_path = File.join(*path_array)
+        puts "autoloading #{require_path}"
+
+        begin
+          require require_path
+          klass = const_get(name)
+          return klass if klass
+        rescue LoadError=>e
+        end
+        
+      end
+
       raise "Class not found: #{name}"
     end
   end
