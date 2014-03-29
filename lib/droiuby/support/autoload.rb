564f280201c2938bc93e451dac1c91879741b9a2
Joseph Emmanuel Dayo <joseph.dayo@gmail.com>
2014-03-29 15:18:13 +0800

    update the spec templates

--- lib/droiuby/support/autoload.rb
+++ lib/droiuby/support/autoload.rb
@@ -1,22 +1,21 @@
 $autoload_path = [nil,File.join('droiuby','wrappers')]
 
-
 module MissingHandler
   def const_missing(name)
     @looked_for ||= {}
     str_name = name.to_s
     raise "Class not found: #{name}" if @looked_for[str_name] == :missing
-    
-    
+
+
     name_parts = name.to_s.split('::').collect { |n| n.underscore }
-    
-    
+
+
     $autoload_path.each do |path|
-      path_array = unless path.nil? 
+      path_array = unless path.nil?
         [path] + name_parts
       else
         name_parts
-      end 
+      end
       require_path = File.join(*path_array)
 
       begin
@@ -25,9 +24,9 @@ module MissingHandler
         return klass if klass
       rescue LoadError=>e
       end
-      
+
     end
-    
+
     @looked_for[str_name] = :missing
 
     raise "Class not found: #{name}"
@@ -38,7 +37,7 @@ end
 class Object
   class << self
     alias :const_missing_old :const_missing
-    
+
     include MissingHandler
   end
 end
